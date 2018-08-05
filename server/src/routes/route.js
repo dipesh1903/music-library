6bba69d03d07884a7e77b9dcf2b1096c86003afa
dipesh1903 <dipesh1903@gmail.com>
2018-08-05 20:56:57 +0530

    added controller

--- server/src/routes/route.js
+++ server/src/routes/route.js
@@ -1,6 +1,6 @@
+const authenticationController = require('../controller/authenticationController')
 
 module.exports = (app) => {
-  app.get('/register', (req, res) => {
-    res.send('Welcome to full stack developer guide')
-  })
+  app.get('/register', authenticationController.register
+  )
 }
